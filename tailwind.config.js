735647c36335ef137f572c778459227c602ffba0
Petruxchinedu <dominicpeter494@gmail.com>
2023-11-06 05:12:05 +0100

    quiz app with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'image': "url(src/asset/images (28).jpeg)"
+        // 'hero-pattern': "url('/img/hero-pattern.svg')",
+    },
+  },
+  plugins: [],
+}
+}
